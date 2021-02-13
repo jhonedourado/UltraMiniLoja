77657e5de64a3e88238d15915a5fd71e7b93614e
Jhone Dourado <72326178+jhonedourado@users.noreply.github.com>
2021-02-13 15:10:20 -0300

    Adicionei os arquivos via upload.

---
+++ produtos.js
@@ -0,0 +1,56 @@
+const items = [
+    {
+        id: 0,
+        quantidade: 0,
+        nome: 'Blusa Masculina Cinza',
+        img: './blusacinza.jpg',
+        preco: 'R$ 35,99',
+        dividido: '12x de R$ 00,00 sem juros',
+        descricao: 'Aqui vai a descrição aqui vai a descrição aqui vai a descrição aqui vai a descrição'
+    },
+    {
+        id: 1,
+        quantidade: 0,
+        nome: 'Blusa Masculina Cinza',
+        img: './blusacinza.jpg',
+        preco: 'R$ 35,99',
+        dividido: '12x de R$ 00,00 sem juros',
+        descricao: 'Aqui vai a descrição aqui vai a descrição aqui vai a descrição aqui vai a descrição'
+    },
+    {
+        id: 2,
+        quantidade: 0,
+        nome: 'Blusa Masculina Cinza',
+        img: './blusacinza.jpg',
+        preco: 'R$ 35,99',
+        dividido: '12x de R$ 00,00 sem juros',
+        descricao: 'Aqui vai a descrição aqui vai a descrição aqui vai a descrição aqui vai a descrição'
+    },
+    {
+        id: 3,
+        quantidade: 0,
+        nome: 'Blusa Masculina Cinza',
+        img: './blusacinza.jpg',
+        preco: 'R$ 35,99',
+        dividido: '12x de R$ 00,00 sem juros',
+        descricao: 'Aqui vai a descrição aqui vai a descrição aqui vai a descrição aqui vai a descrição'
+    },
+    {
+        id: 4,
+        quantidade: 0,
+        nome: 'Blusa Masculina Cinza',
+        img: './blusacinza.jpg',
+        preco: 'R$ 35,99',
+        dividido: '12x de R$ 00,00 sem juros',
+        descricao: 'Aqui vai a descrição aqui vai a descrição aqui vai a descrição aqui vai a descrição'
+    },
+    {
+        id: 5,
+        quantidade: 0,
+        nome: 'Blusa Masculina Cinza',
+        img: './blusacinza.jpg',
+        preco: 'R$ 35,99',
+        dividido: '12x de R$ 00,00 sem juros',
+        descricao: 'Aqui vai a descrição aqui vai a descrição aqui vai a descrição aqui vai a descrição'
+    }
+]
